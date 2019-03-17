3a176ec2fcd2e0a43da557f79f54a6f3a07408e0
Jørn Lode <jlode90@gmail.com>
2019-03-17 19:43:57 +0100

    Another experimental commit

--- fibaro-fgbs-222.groovy
+++ fibaro-fgbs-222.groovy
@@ -1,39 +1,132 @@
 metadata {
-	definition (name: "Fibaro FGBS-222 Smart Implant", namespace: "hubitat", author: "Jørn Lode") {
-		// TODO Register as composite device
-		capability "Actuator"
+	definition (name: "Fibaro FGBS-222 Smart Implant", namespace: "boblehest", author: "Jørn Lode") {
 		capability "Configuration"
-		capability "Sensor"
-		capability "Switch"
 
-		// TODO add fingerprint
+		fingerprint deviceId: "4096", inClusters: "0x5E,0x25,0x85,0x8E,0x59,0x55,0x86,0x72,0x5A,0x73,0x98,0x9F,0x5B,0x31,0x60,0x70,0x56,0x71,0x75,0x7A,0x6C,0x22"
 	}
 
 	preferences {
 		generate_preferences(configuration_model())
 	}
+}
 
-	tiles {
-		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-			state "off", label: '${name}', action: "on",
-				icon: "st.switches.switch.off", backgroundColor: "#ffffff"
-			state "on", label: '${name}', action: "off",
-				icon: "st.switches.switch.on", backgroundColor: "#00a0dc"
-		}
+def installed() {
+	log.debug "installed"
+	initialize()
+}
 
-		main "switch"
+def updated() {
+	log.debug "updated"
+	initialize()
+}
 
-		details ["switch"]
+private initialize() {
+	if (!childDevices) {
+		addChildDevices()
+	}
+
+	// TODO Testing
+	// multiChannelAssociationV2.multiChannelAssociationSet(
+
+	// toEndpoint(zwave.associationV2.associationSet(
+	// 	groupingIdentifier: 2,
+	// 	nodeId: zwaveHubNodeId
+	// 	), 5).format()
+
+	formatCommands(1..3.collect {
+		zwave.associationV2.associationGet(
+			groupingIdentifier: it)
+	}, 500)
+}
+
+// ----------------------------------------------------------------------------
+// ------------------------------- CHILD DEVICES ------------------------------
+// ----------------------------------------------------------------------------
+
+private childNetworkId(ep) {
+	"${device.deviceNetworkId}-ep${ep}"
+}
+
+private addChildDevices() {
+	try {
+		addChildSwitches()
+	} catch (e) {
+		sendEvent(
+			descriptionText: "Child device creation failed.",
+			eventType: "ALERT",
+			name: "childDeviceCreation",
+			value: "failed",
+			displayed: true,
+		)
 	}
 }
 
-def on() {
-	encap(hubitat.zwave.commands.switchbinaryv1.SwitchBinarySet(switchValue: 0xff), 5)
+// -------- Digital inputs (EP 1 & 2) --------
+
+// TODO Add as configured
+// private addChildDigitalInputs() {
+// 	1..2.eachWithIndex { ep, index ->
+// 		addChildDevice("Fibaro FGBS-222 Child Input",
+// 			childNetworkId(ep), componentLabel: "Input ${index+1} - Digital")
+// 	}
+// }
+
+// -------- Analog inputs (EP 3 & 4) --------
+
+// TODO Add as configured
+// private addChildAnalogInputs() {
+// 	3..4.eachWithIndex { ep, index ->
+// 		addChildDevice("Fibaro FGBS-222 Child Analog Input",
+// 			childNetworkId(ep), componentLabel: "Input ${index+1} - Analog")
+// 	}
+// }
+
+// -------- Outputs (EP 5 & 6) --------
+
+private addChildSwitches() {
+	5..6.eachWithIndex { ep, index ->
+		addChildDevice("Fibaro FGBS-222 Child Switch",
+			childNetworkId(ep), componentLabel: "Output ${index+1}")
+	}
 }
 
-def off() {
-	encap(hubitat.zwave.commands.switchbinaryv1.SwitchBinarySet(switchValue: 0), 5)
+private setSwitch(value, channel) {
+	// TODO Should we also send a Get request? How often? Can't we have the
+	// device notify us whenever it changes?
+	toEndpoint(zwave.switchBinaryV1.switchBinarySet(switchValue: value), channel).format()
 }
+
+def childOn(String dni) {
+	// formatCommands([
+	// 	toEndpoint(zwave.switchBinaryV1.switchBinarySet(switchValue: 0xff), 5),
+	// 	toEndpoint(zwave.basicV1.basicGet(), 5),
+	// 	toEndpoint(zwave.basicV1.basicGet(), 5),
+	// 	toEndpoint(zwave.sensorMultilevelV5.sensorMultilevelGet(sensorType: 1), 7), 
+	// ], 2000)
+
+	setSwitch(0xff, channelNumber(dni))
+}
+
+def childOff(String dni) {
+	setSwitch(0, channelNumber(dni))
+}
+
+def childRefresh(String dni) {
+	toEndpoint(zwave.switchBinaryV1.switchBinaryGet(), channelNumber(dni)).format()
+}
+
+// -------- Temperature sensors (EP 7-13) --------
+
+// TODO Add external sensors as configured
+// private addChildTemperatureSensors() {
+// 	def ep = 7
+// 	addChildDevice("Fibaro FGBS-222 Child Temperature Sensor",
+// 		childNetworkId(ep), componentLabel: "Internal temperature sensor")
+// }
+
+// ----------------------------------------------------------------------------
+// ----------------------------- MESSAGE PARSING ------------------------------
+// ----------------------------------------------------------------------------
 
 def parse(String description) {
 	def result = []
@@ -49,77 +142,113 @@ def parse(String description) {
 	result
 }
 
-// TODO Write code to sync configuration and other state
-def configure() {
-	// def configuration = new XmlSlurper().parseText(configuration_model())
-}
-
-def zwaveEvent(hubitat.zwave.commands.multichannelv3.MultiChannelCmdEncap cmd) {
+private zwaveEvent(hubitat.zwave.commands.multichannelv3.MultiChannelCmdEncap cmd) {
+	// TODO Figure out what the parameter to `encapsulatedCommand` does.
+	// Try removing the argument.
 	def encapsulatedCommand = cmd.encapsulatedCommand([0x25: 1, 0x20: 1])
 	if (encapsulatedCommand) {
-		zwaveEvent(encapsulatedCommand, cmd.sourceEndPoint as Integer)
+		// TODO Is sourceEndPoint really not an integer?
+		zwaveEvent(encapsulatedCommand, cmd.sourceEndPoint.toInteger())
 	} else {
 		log.debug "Ignored encapsulated command: ${cmd}"
 	}
 }
 
-def zwaveEvent(hubitat.zwave.commands.switchbinaryv1.SwitchBinarySet cmd, ep=null) {
-	// if (!ep in [5, 6]) {
-	if (ep != 5) {
-		return;
+private zwaveEvent(hubitat.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd, ep) {
+	def target = childDevices.find { it.deviceNetworkId == childNetworkId(ep) }
+	// TODO Why not createEvent? Try it.
+	childDevice?.sendEvent(name: "switch", value: cmd.value ? "on" : "off")
+}
+
+private zwaveEvent(hubitat.zwave.commands.configurationv1.ConfigurationReport cmd) {
+	// TODO Are all settings properly set? Monitor the logs.
+	def configuration = new XmlSlurper().parseText(configuration_model())
+
+	def paramName = cmd.parameterNumber.toString()
+	def parameterInfo = configuration.Value.find { it.@index == paramName }
+	def byteSize = sizeOfParameter(parameterInfo)
+	if (byteSize != cmd.size) {
+		log.debug "Parameter ${paramName} has unexpected size ${cmd.size} (expected ${byteSize})"
 	}
 
-	createEvent(name: "switch", value: cmd.switchValue);
-}
+	def remoteValue = bytesToInteger(cmd.configurationValue)
+	def localValue = settings[cmd.parameterNumber.toString()]
 
-def zwaveEvent(hubitat.zwave.commands.basicv1.BasicSet cmd, ep=null) {
-	// if (!ep in [5, 6]) {
-	if (ep != 5) {
-		return;
-	}
-
-	createEvent(name: "switch", value: cmd.value);
-}
-
-def zwaveEvent(hubitat.zwave.Command cmd) {
-	// log.debug "Unhandled event $cmd"
-	createEvent(descriptionText: "${device.displayName}: ${cmd}")
-}
-
-private encap(cmd, endpoint) {
-	if (endpoint) {
-		zwave.multiChannelV3.multiChannelCmdEncap(destinationEndPoint:endpoint).encapsulate(cmd)
-	} else {
-		cmd
+	if (localValue != remoteValue) {
+		log.debug "Parameter ${paramName} has value ${remoteValue} after trying to set it to ${localValue}"
 	}
 }
 
-def generate_preferences(configuration_model) {
+// private zwaveEvent(hubitat.zwave.commands.sensormultilevelv5.SensorMultilevelReport cmd, ep) {
+// 	log.debug "Sensor @ endpoint ${ep} has value ${cmd.scaledSensorValue}"
+// 	createEvent(name: "temperature", value: cmd.scaledSensorValue)
+// }
+
+// private zwaveEvent(hubitat.zwave.commands.basicv1.BasicReport cmd, ep) {
+// 	log.debug "Switch @ endpoint ${ep} has value ${cmd.value}"
+// 	createEvent(name: "switch", value: cmd.value == 0 ? "off" : "on")
+// }
+
+private zwaveEvent(hubitat.zwave.Command cmd, ep=null) {
+	log.debug "Unhandled event ${cmd} (endpoint ${ep})"
+	// createEvent(descriptionText: "${device.displayName}: ${cmd}")
+}
+
+// ----------------------------------------------------------------------------
+// ------------------------------ CONFIGURATION -------------------------------
+// ----------------------------------------------------------------------------
+
+def configure() {
+	def configuration = new XmlSlurper().parseText(configuration_model())
+	def cmds = []
+
+	configuration.Value.each {
+		def settingValue = settings[it.@index.toString()].toInteger()
+		def byteSize = sizeOfParameter(it)
+
+		if (settingValue != null) {
+			if (settingValue == "") {
+				log.debug "Setting ${it.@index} is empty"
+			}
+
+			def index = it.@index.toInteger()
+			cmds << zwave.configurationV1.configurationSet(configurationValue: integerToBytes(settingValue, byteSize), parameterNumber: index, size: byteSize)
+			cmds << zwave.configurationV1.configurationGet(parameterNumber: index)
+		} else {
+			log.debug "Setting ${it.@index} has null value"
+		}
+	}
+	
+	log.debug "cmds: ${cmds}"
+	
+	formatCommands(cmds)
+}
+
+private generate_preferences(configuration_model) {
 	def configuration = new XmlSlurper().parseText(configuration_model)
 
-	configuration.Value.each
-	{
-		switch(it.@type)
-		{
-			case ["byte","short","four"]:
+	configuration.Value.each {
+		switch(it.@type) {
+			case ["byte", "short", "four"]:
 				input "${it.@index}", "number",
-					title:"${it.@label}\n" + "${it.Help}",
+					title: "${it.@label}\n" + "${it.Help}",
 					range: "${it.@min}..${it.@max}",
 					defaultValue: "${it.@value}",
 					displayDuringSetup: "${it.@displayDuringSetup}"
 				break
 			case "list":
-				def items = []
-				it.Item.each { items << ["${it.@value}":"${it.@label}"] }
+				// def items = []
+				// it.Item.each { items << ["${it.@value}": "${it.@label}"] }
+				def items = it.Item.collect { ["${it.@value}": "${it.@label}"] }
 				input "${it.@index}", "enum",
-					title:"${it.@label}\n" + "${it.Help}",
+					title: "${it.@label}\n" + "${it.Help}",
 					defaultValue: "${it.@value}",
 					displayDuringSetup: "${it.@displayDuringSetup}",
 					options: items
 				break
 			case "decimal":
 				input "${it.@index}", "decimal",
-					title:"${it.@label}\n" + "${it.Help}",
+					title: "${it.@label}\n" + "${it.Help}",
 					range: "${it.@min}..${it.@max}",
 					defaultValue: "${it.@value}",
 					displayDuringSetup: "${it.@displayDuringSetup}"
@@ -140,7 +269,7 @@ def generate_preferences(configuration_model) {
 	}
 }
 
-def configuration_model() {
+private configuration_model() {
 	'''
 <configuration>
 	<Value type="list" genre="config" instance="1" index="20" label="Input 1 - operating mode" value="2" size="1">
@@ -369,4 +498,40 @@ def configuration_model() {
 	</Value>
 </configuration>
 	'''
+}
+
+// ----------------------------------------------------------------------------
+// --------------------------------- HELPERS ----------------------------------
+// ----------------------------------------------------------------------------
+
+private toEndpoint(cmd, endpoint) {
+	zwave.multiChannelV3.multiChannelCmdEncap(destinationEndPoint: endpoint)
+		.encapsulate(cmd)
+}
+
+private formatCommands(cmds, delay=null) {
+	def formattedCmds = cmds.collect { it.format() }
+
+	if (delay) {
+		delayBetween(formattedCmds, delay)
+	} else {
+		formattedCmds
+	}
+}
+	
+private bytesToInteger(array) {
+	array.inject(0) { result, i -> (result << 8) | i }
+}
+
+private integerToBytes(value, length) {
+	(length-1..0).collect { (value >> (it * 8)) & 0xFF }
+}
+
+private sizeOfParameter(paramData) {
+	def typeSizes = [short: 2, four: 4, list: paramData.@size]
+	typeSizes[paramData.@type] ?: 1
+}
+
+private channelNumber(String dni) {
+	dni.split("-ep")[-1].toInteger()
 }
